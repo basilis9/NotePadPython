7798899ac3c8a73f165d7c7c7d8726e8a875cf25
basilis9 <baskoletsis@gmail.com>
2024-06-03 18:31:38 +0300

    Add files via upload

---
+++ NotePad.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+time.sleep(1)
+pyautogui.press("winleft")
+time.sleep(1)
+pyautogui.write("notepad")
+time.sleep(1)
+pyautogui.press("Enter")
+time.sleep(1)
+pyautogui.write("HAHAHAHAHAHHAHAHAH")
+pyautogui.hotkey("Alt","F4")
+time.sleep(1)
+pyautogui.press("tab")
+time.sleep(1)
+pyautogui.press("Enter")
